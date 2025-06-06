9596b4cf2996b30de7ce478628513ebdbcd50fbe
Piero Arturo <piero.dev@outlook.com>
2025-06-06 16:51:43 -0500

    Listado de Incidencias estilado inicial

--- controllers/controladorIncidencias.js
+++ controllers/controladorIncidencias.js
@@ -3,7 +3,17 @@ const dbIncidencias = require("../model/queriesIncidencias")
 async function obtenerIncidencias(req, res) {
   try {
     const incidencias = await dbIncidencias.obtenerIncidencias()
-    res.render("incidencias", { incidencias, title: "Incidencias" })
+
+    const procesadas = incidencias.map(inc => ({
+      ...inc,
+      detalle_productos: Array.isArray(inc.detalle_productos)
+        ? inc.detalle_productos
+        : JSON.parse(inc.detalle_productos)
+    }));
+
+    console.log(procesadas)
+
+    res.render("incidencias", { incidencias: procesadas, title: "Incidencias" })
   } catch (error) {
     console.error("Error al obtener categorias:", error)
     res.status(500).send("Error al obtener las categorias")

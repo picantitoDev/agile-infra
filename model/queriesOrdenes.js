23570925813dc7f0cee513d6cec8f9b5e6ae1699
Piero Arturo <piero.dev@outlook.com>
2025-06-25 11:43:53 -0500

    po r f a v or

--- model/queriesOrdenes.js
+++ model/queriesOrdenes.js
@@ -122,23 +122,22 @@ async function obtenerOrdenesUltimos30Dias() {
 }
 
 
-async function obtenerDetalleOrdenesPorFecha(fecha) {
-  const fechaInicio = `${fecha}T00:00:00.000Z`;
-  const fechaFin = `${fecha}T23:59:59.999Z`;
-
-  const { rows } = await pool.query(`
-    SELECT 
-      o.id_order,
-      o.fecha,
-      o.estado,
-      o.products,
-      pr.razon_social AS proveedor
+async function obtenerDetalleOrdenesPorFecha(fechaLimaStr) {
+  const result = await pool.query(`
+    SELECT o.*, p.nombre AS proveedor, json_agg(json_build_object(
+      'nombre', pr.nombre,
+      'cantidad', po.cantidad
+    )) AS products
     FROM orden_reabastecimiento o
-    JOIN proveedor pr ON pr.id_proveedor = o.id_proveedor
-    WHERE o.fecha BETWEEN $1 AND $2
-  `, [fechaInicio, fechaFin]);
+    JOIN proveedor p ON o.id_proveedor = p.id_proveedor
+    JOIN producto_orden po ON po.id_orden = o.id_order
+    JOIN producto pr ON po.id_producto = pr.id_producto
+    WHERE (o.fecha AT TIME ZONE 'UTC' AT TIME ZONE 'America/Lima')::DATE = $1
+    GROUP BY o.id_order, p.nombre
+    ORDER BY o.fecha;
+  `, [fechaLimaStr]);
 
-  return rows;
+  return result.rows;
 }
 
 module.exports = {

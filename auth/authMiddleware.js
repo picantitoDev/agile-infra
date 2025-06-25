f83ef3bb895debbf50beb3f755f005ef727c035f
Piero Arturo <piero.dev@outlook.com>
2025-06-25 17:31:59 -0500

    plissss

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,8 +1,15 @@
 function verificarSesion(req, res, next) {
   if (req.isAuthenticated()) {
-    return next()
+    if (req.user.estado !== 'Activado') {
+      req.logout(() => {
+        req.flash('error_msg', 'Tu cuenta ha sido desactivada.');
+        return res.redirect('/');
+      });
+    } else {
+      return next();
+    }
+  } else {
+    return res.redirect('/');
   }
-  res.redirect("/")
 }
-
 module.exports = verificarSesion
